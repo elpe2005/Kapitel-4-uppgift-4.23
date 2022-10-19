a80dc85bf299f421522bdaec9f3372820dfc0933
oskr05 <oskr05001@utb.vaxjo.se>
2022-10-19 12:11:47 +0200

    Add project files.

---
+++ Kapitel 4 uppgift 4.23/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Uppgift423
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Skriv in ett Heltal");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vill du skriva ett till nummer eller avsluta programmet");
+            string bn (Cosnole.)
+            string nn
+            {
+                while ( = n1) ;
+            }
+        }
+    }
+}
+
